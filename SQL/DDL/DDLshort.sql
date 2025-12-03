7b489c02a4841e6daca4809d69e12d9f4170ecac
MiguelAngel59 <159050193+MiguelAngel59@users.noreply.github.com>
2025-12-03 03:03:42 -0600

    Update DDLshort.sql

--- SQL/DDL/DDLshort.sql
+++ SQL/DDL/DDLshort.sql
@@ -1,7 +1,6 @@
-
 -- TABLA: AEROPUERTO
 CREATE TABLE aeropuerto (
-    id_aeropuerto SERIAL PRIMARY KEY,
+    id_aeropuerto INT PRIMARY KEY,
     nombre VARCHAR(100) NOT NULL,
     tipo VARCHAR(15) NOT NULL CHECK (tipo IN ('INTERNACIONAL', 'NACIONAL')),
     pais VARCHAR(80) NOT NULL,
@@ -14,7 +13,7 @@ COMMENT ON TABLE aeropuerto IS 'Tabla que almacena la información de los aeropu
 
 -- TABLA: TERMINAL
 CREATE TABLE terminal (
-    id_terminal SERIAL PRIMARY KEY,
+    id_terminal INT PRIMARY KEY,
     id_aeropuerto INT NOT NULL REFERENCES aeropuerto(id_aeropuerto),
     numero_terminal INT NOT NULL,
     nombre VARCHAR(50),
@@ -25,8 +24,9 @@ COMMENT ON TABLE terminal IS 'Terminales pertenecientes a los aeropuertos';
 
 
 
+-- TABLA: PUERTA
 CREATE TABLE puerta (
-    id_puerta SERIAL PRIMARY KEY,
+    id_puerta INT PRIMARY KEY,
     id_terminal INT NOT NULL REFERENCES terminal(id_terminal),
     numero_puerta INT NOT NULL,
     codigo VARCHAR(10) NOT NULL,
@@ -37,22 +37,22 @@ COMMENT ON TABLE puerta IS 'Puertas/gates pertenecientes a una terminal de un ae
 
 
 
--- PISTA
+-- TABLA: PISTA
 CREATE TABLE pista (
-    id_pista SERIAL PRIMARY KEY,
+    id_pista INT PRIMARY KEY,
     id_aeropuerto INT NOT NULL REFERENCES aeropuerto(id_aeropuerto),
     codigo VARCHAR(10) NOT NULL,
     longitud_m INT CHECK (longitud_m > 0),
     estado VARCHAR(20) NOT NULL CHECK (estado IN ('OPERATIVA', 'CERRADA', 'MANTENIMIENTO')),
-
     UNIQUE (id_aeropuerto, codigo)
 );
 COMMENT ON TABLE pista IS 'Pistas del aeropuerto';
 
 
+
 -- TABLA: AEROLÍNEA
 CREATE TABLE aerolinea (
-    id_aerolinea SERIAL PRIMARY KEY,
+    id_aerolinea INT PRIMARY KEY,
     nombre VARCHAR(100) NOT NULL,
     pais_origen VARCHAR(80) NOT NULL,
     codigo_internacional CHAR(3) NOT NULL UNIQUE,
@@ -61,32 +61,35 @@ CREATE TABLE aerolinea (
 COMMENT ON TABLE aerolinea IS 'Información de las aerolíneas';
 
 
+
 -- TABLA: AVIÓN
 CREATE TABLE avion (
-    id_avion SERIAL PRIMARY KEY,
+    id_avion INT PRIMARY KEY,
     id_aerolinea INT NOT NULL REFERENCES aerolinea(id_aerolinea),
-    id_aeropuerto INT REFERENCES aeropuerto(id_aeropuerto), -- ubicación actual cuando está en tierra (nullable)
+    id_aeropuerto INT REFERENCES aeropuerto(id_aeropuerto),
     matricula VARCHAR(20) UNIQUE NOT NULL,
     modelo VARCHAR(35) NOT NULL,
-    estado_avion VARCHAR(20) NOT NULL DEFAULT 'OPERATIVO', -- OPERATIVO, MANTENIMIENTO, RETIRADO
+    estado_avion VARCHAR(20) NOT NULL DEFAULT 'OPERATIVO',
     capacidad_pasajeros INT CHECK (capacidad_pasajeros >= 0)
 );
 COMMENT ON TABLE avion IS 'Información de los aviones';
 
 
+
 -- TABLA: EMPLEADO
 CREATE TABLE empleado (
-    id_empleado SERIAL PRIMARY KEY,
+    id_empleado INT PRIMARY KEY,
     id_aerolinea INT REFERENCES aerolinea(id_aerolinea),
     id_aeropuerto INT REFERENCES aeropuerto(id_aeropuerto),
     nombre VARCHAR(60) NOT NULL,
     apellido_paterno VARCHAR(60) NOT NULL,
     apellido_materno VARCHAR(60),
     fecha_nacimiento DATE NOT NULL,
-    nacionalidad VARCHAR(60) NOT NULL,
-    -- tipo_empleado VARCHAR(30) NOT NULL -- 'PILOTO','SOBRECARGO','CONTROLADOR','TECNICO','ATENCION','INGENIERO'
+    nacionalidad VARCHAR(60) NOT NULL
 );
 COMMENT ON TABLE empleado IS 'Supertipo para empleados';
+
+
 
 -- TABLAS DE EMPLEADOS
 CREATE TABLE piloto (
@@ -94,6 +97,7 @@ CREATE TABLE piloto (
     horas_vuelo INT CHECK (horas_vuelo >= 0)
 );
 COMMENT ON TABLE piloto IS 'Pilotos';
+
 
 
 CREATE TABLE controlador (
@@ -104,17 +108,23 @@ CREATE TABLE controlador (
 );
 COMMENT ON TABLE controlador IS 'Controladores';
 
+
+
 CREATE TABLE sobrecargo (
     id_empleado INT PRIMARY KEY REFERENCES empleado(id_empleado) ON DELETE CASCADE,
     anos_experiencia INT CHECK (anos_experiencia >= 0)
 );
 COMMENT ON TABLE sobrecargo IS 'Sobrecargos';
 
+
+
 CREATE TABLE tecnico (
     id_empleado INT PRIMARY KEY REFERENCES empleado(id_empleado) ON DELETE CASCADE,
     especialidad VARCHAR(50)
 );
 COMMENT ON TABLE tecnico IS 'Técnicos';
+
+
 
 CREATE TABLE ingeniero (
     id_empleado INT PRIMARY KEY REFERENCES empleado(id_empleado) ON DELETE CASCADE,
@@ -123,38 +133,46 @@ CREATE TABLE ingeniero (
 );
 COMMENT ON TABLE ingeniero IS 'Ingenieros';
 
+
+
 CREATE TABLE atencion_pasajero (
     id_empleado INT PRIMARY KEY REFERENCES empleado(id_empleado) ON DELETE CASCADE,
     puesto VARCHAR(50)
 );
 COMMENT ON TABLE atencion_pasajero IS 'Atención a pasajeros';
 
--- LICENCIAS de piloto
+
+
+-- TABLA: LICENCIA PILOTO
 CREATE TABLE licencia_piloto (
-    id_licencia SERIAL PRIMARY KEY,
+    id_licencia INT PRIMARY KEY,
     id_piloto INT NOT NULL REFERENCES piloto(id_empleado) ON DELETE CASCADE,
-    tipo_licencia VARCHAR(50) NOT NULL, -- p.e. ATPL, CPL, HabilitacionA320
+    tipo_licencia VARCHAR(50) NOT NULL,
     numero_licencia VARCHAR(50) NOT NULL,
     fecha_emision DATE NOT NULL,
     fecha_vencimiento DATE NOT NULL,
     autoridad_emisora VARCHAR(80)
 );
 
+
+
 -- TABLA: VUELO
 CREATE TABLE vuelo (
-    id_vuelo SERIAL PRIMARY KEY,
+    id_vuelo INT PRIMARY KEY,
     codigo_vuelo VARCHAR(10) NOT NULL UNIQUE,
     origen INT NOT NULL REFERENCES aeropuerto(id_aeropuerto),
     destino INT NOT NULL REFERENCES aeropuerto(id_aeropuerto),
-    tipo VARCHAR(20) NOT NULL,  -- comercial, internacional, privado, etc.
-    estado VARCHAR(20) NOT NULL, -- programado, en vuelo, completado, cancelado
+    tipo VARCHAR(20) NOT NULL,
+    estado VARCHAR(20) NOT NULL,
     tiempo_salida TIMESTAMP,
     tiempo_llegada TIMESTAMP
 );
 
 
+
+-- TABLA: PROGRAMACION VUELO
 CREATE TABLE programacion_vuelo (
-    id_programacion SERIAL PRIMARY KEY,
+    id_programacion INT PRIMARY KEY,
     id_vuelo INT NOT NULL REFERENCES vuelo(id_vuelo),
     id_avion INT NOT NULL REFERENCES avion(id_avion),
     id_piloto INT NOT NULL REFERENCES piloto(id_empleado),
@@ -165,19 +183,22 @@ CREATE TABLE programacion_vuelo (
 );
 COMMENT ON TABLE programacion_vuelo IS 'Programación planificada de un vuelo (ETD, ETA, avión, piloto)';
 
+
+
 -- TABLA: TARIFA_VUELO
 CREATE TABLE tarifa_vuelo (
-    id_tarifa SERIAL PRIMARY KEY,
+    id_tarifa INT PRIMARY KEY,
     id_programacion_vuelo INT NOT NULL REFERENCES programacion_vuelo(id_programacion),
     clase VARCHAR(20) NOT NULL,
     precio NUMERIC(12,2) NOT NULL CHECK (precio >= 0),
-    UNIQUE (id_programacion, clase)
+    UNIQUE (id_programacion_vuelo, clase)
 );
+
 
 
 -- TABLA: BOLETO
 CREATE TABLE boleto (
-    id_boleto SERIAL PRIMARY KEY,
+    id_boleto INT PRIMARY KEY,
     id_programacion_vuelo INT NOT NULL REFERENCES programacion_vuelo(id_programacion),
     id_tarifa INT NOT NULL REFERENCES tarifa_vuelo(id_tarifa),
     fecha_compra DATE NOT NULL,
